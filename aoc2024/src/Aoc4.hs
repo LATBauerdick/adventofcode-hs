fd391830fbb5eabfad89f9dbaa992b4f51d40078
LATBauerdick <github@bauerdick.org>
2024-12-17 17:48:55 -0600

    2024-12-17

--- aoc2024/src/Aoc4.hs
+++ aoc2024/src/Aoc4.hs
@@ -1,7 +1,8 @@
 module Aoc4 (aoc4) where
 
+import Data.List as L (length)
 import Data.Maybe (fromJust)
-import qualified Data.Text as T (breakOn, breakOnAll, drop, filter, intercalate, lines, splitOn)
+import qualified Data.Text as T (breakOn, breakOnAll, drop, filter, intercalate, length, lines, splitOn, transpose)
 
 debug :: a -> Text -> a
 debug a t = flip trace a (toString t)
@@ -19,9 +20,28 @@ readMaybeInts :: Text -> [Maybe Int]
 readMaybeInts l = map readMaybeInt $ T.splitOn " " l
 
 -------------------------------------------------------------------
+
+parseLine :: Text -> Int
+parseLine l = n
+ where
+  n = n0 + n1
+  n0 = L.length . T.breakOnAll "XMAS" $ l
+  n1 = L.length . T.breakOnAll "SAMX" $ l
+
 aoc4 :: IO (Int, Int)
 aoc4 = do
   ls <- slurp "data/aoc4.dat" <&> take 10
-  let a = 0
+  print ls
+  let ll = T.length . fromJust . viaNonEmpty head $ ls
+      nl = length ls
+      tl = nl * ll
+  let vs = T.transpose ls
+  print vs
+  let xs = [[(min j (ll - 1), min i (nl - 1)) | j <- [0 .. i]] | i <- [0 .. (ll + nl - 1)]]
+  print xs
+  print . fmap parseLine $ ls
+  print . fmap parseLine $ vs
+
+  let a = sum $ (fmap parseLine ls) <> (fmap parseLine vs)
   let b = 0
   pure (a, b)
